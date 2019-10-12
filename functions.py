df11ec886d03a8ebb9fae5db7351214ade82bdc6
Jiayi Lu <215756347@qq.com>
2019-10-12 16:02:05 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -15,6 +15,77 @@ import matplotlib.cm as cm
 import uuid
 import cv2
 
+# gray 
+def visualize_saliency_gray(model, file_folder, output_folder):
+    Font1 = ImageFont.truetype("C:\Windows\Fonts\simsunb.ttf",36)
+    
+    text_color = (255,255,255)
+    
+    layer_idx = utils.find_layer_idx(model, model.layers[-1].name)
+    CLASS_NUM = model.layers[-1].output_shape[1]
+    
+    width = model.layers[0].input.get_shape()[1].value
+    height= model.layers[0].input.get_shape()[2].value
+    assert(width == height)
+    
+    img = np.array(Image.open(file_path))/255
+    img = np.expand_dims(img,2)
+    img = np.expand_dims(img,0)         
+    img_rgb_bg = np.array(Image.open(file_path).convert("RGB"))/255     
+   
+    print(model.predict_classes(img))
+    prob = model.predict_proba(img).round(3)
+    print(prob)
+
+    GRADS = []
+    MAX = np.array([])        
+    for index in range(CLASS_NUM):
+            grads = visualize_saliency(model, layer_idx, filter_indices=index,
+                                   seed_input=img, backprop_modifier="guided")
+            MAX = np.append(MAX, grads.max())
+            GRADS.append(grads)
+    MAX_PIXEL = MAX.max()
+        
+    if MAX_PIXEL == 0:
+        MAX_PIXEL = 0.00001
+        
+    L = []
+    for i, grads in enumerate(GRADS):
+            # 2
+            #grads[0,0] = MAX_PIXEL
+            grads = grads/MAX_PIXEL        
+            cmap = plt.get_cmap('jet')
+            rgba_img = cmap(grads)
+            rgb_img = np.delete(rgba_img, 3, 2)
+            #3 
+            jet_heatmap = np.uint8(cm.jet(grads)[...,:3] * 255)
+            Overlay = overlay(jet_heatmap, img_rgb_bg*255)
+            L.append(np.concatenate((img_rgb_bg, rgb_img, Overlay/255), axis = 1))
+    TMP = []
+    for i, IM in enumerate(L):
+            if i == 0:
+                TMP = IM
+            else:
+                TMP = np.concatenate((TMP, IM), axis = 0)
+    # vertical lines
+    for i in range(3):
+        TMP = cv2.line(TMP, (i*width,0), (i*width ,width*3),  color = (1, 1, 1), thickness = 1)
+    # horizontal lines
+    for i in range(CLASS_NUM):
+        TMP = cv2.line(TMP, (0, i*width), (width*3, i*width),  color = (1, 1, 1), thickness = 1)
+
+    im = Image.fromarray(np.uint8(TMP*255))
+    draw = ImageDraw.Draw(im)
+    for i in range(CLASS_NUM):
+        draw.text((0 + 5, i*width + 5),  str(i), fill = text_color, font=Font1)
+        draw.text((0 + 5 + width, i*width + 5), str(prob[0][i]), fill = text_color, font=Font1)
+
+    uuid_str = uuid.uuid4().hex
+    im.save(output_folder+uuid_str+'.jpg')
+
+
+
+
 # label and save a single image for all class
 def label_and_save_contrast(model, file_path, output_folder, CLASS_NUM, layer_idx, width = 96, COLOR_MODE = "RGB", text_color = (255,255,255)):
     
